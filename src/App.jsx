1851660aa46423363de15b369c41fe1b0f2c1901
Rida4142 <waheedrida56@gmail.com>
2025-09-04 13:36:47 +0100

    Add initial UI layout

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import HomeCards from './Components/HomeCards'
+import Joblistings from './Components/Joblistings'
+import ViewAllJobs from './Components/ViewAllJobs'
+const App = () => {
+  return (
+   <>
+   <Navbar />
+   <Hero />
+   <HomeCards />
+   <Joblistings />
+    <ViewAllJobs />
+   
+   </>
+  )
+}
+
+export default App
+
