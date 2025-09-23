01688832580e3f7162d6447f2c7565ce89d16276
Rida4142 <waheedrida56@gmail.com>
2025-09-23 16:36:30 +0500

    Adding add page basic

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import HomePage from './Pages/HomePage'
 import JobsPage from './Pages/JobsPage'
 import NotFoundPage from './Pages/NotFoundPage'
 import JobPage,{jobLoader} from './Pages/JobPage'
+import AddJobPage from './Pages/AddJobPage'
 
 const App = () => {
 
@@ -19,6 +20,7 @@ const App = () => {
     <Route path='/' element={<Mainlayout />}>
        <Route index element={<HomePage />} />
        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/add-job' element={<AddJobPage />} />
        <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader}/>
         <Route path='*' element={<NotFoundPage />} />
     </Route>
--- src/Pages/JobPage.jsx
+++ src/Pages/JobPage.jsx
@@ -59,10 +59,10 @@ const JobPage = () => {
             <div className="bg-white p-6 rounded-lg shadow-md">
               <h3 className="text-xl font-bold mb-6">Company Info</h3>
 
-              <h2 className="text-2xl">NewTek Solutions</h2>
+              <h2 className="text-2xl">{job.company.name}</h2>
 
               <p className="my-2">
-                NewTek Solutions is a leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering a collaborative and innovative work environment.
+               {job.company.description}
               </p>
 
               <hr className="my-4" />
@@ -70,22 +70,22 @@ const JobPage = () => {
               <h3 className="text-xl">Contact Email:</h3>
 
               <p className="my-2 bg-indigo-100 p-2 font-bold">
-                contact@newteksolutions.com
+                {job.company.contactEmail}
               </p>
 
               <h3 className="text-xl">Contact Phone:</h3>
 
-              <p className="my-2 bg-indigo-100 p-2 font-bold">555-555-5555</p>
+              <p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
             </div>
 
             {/* <!-- Manage --> */}
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-              <a
-                href="/add-job.html"
+              <Link
+                to={`/jobs/edits/${job.id}`}
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job</a
-              >
+                >Edit Job
+                </Link>
               <button
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
